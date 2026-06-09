c9c20a187a186725bf412d3d5090c6c663fdd1ac
Marco Correia <mpcorreia4@gmail.com>
2026-06-09 03:05:18 +0300

    Added HomeContent.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,6 @@
-'use client';
-
 import React from 'react';
-import Header from '@/components/Header';
-import HeroSection from '@/components/HeroSection';
-import PropertyListings from '@/components/PropertyListings';
-import ResultsFilterStrip from '@/components/ResultsFilterStrip';
-import ResultsSplitView from '@/components/ResultsSplitView';
-import Footer from '@/components/Footer';
-import { SearchModeProvider, useSearchMode } from '@/contexts/SearchModeContext';
-
-function HomeContent() {
-  const { isResultsViewActive } = useSearchMode();
-
-  if (isResultsViewActive) {
-    return (
-      <div className="min-h-screen bg-white">
-        <Header />
-        <ResultsFilterStrip />
-        <ResultsSplitView />
-      </div>
-    );
-  }
-
-  return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      <HeroSection />
-      <PropertyListings />
-      <Footer />
-    </div>
-  );
-}
+import HomeContent from '@/components/HomeContent';
+import { SearchModeProvider } from '@/contexts/SearchModeContext';
 
 export default function HomePage() {
   return (
